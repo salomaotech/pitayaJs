fe016ff337a768cdfa28613af757e4882f5cad9b
salomaotech <salomao@taimber.com>
2022-12-24 09:57:11 -0300

    LoadComponent.js adicionado identificador de componente, agora cada componente tem sua própria ID e todo o seu código está agrupado

--- public_html/classes/LoadComponent.js
+++ public_html/classes/LoadComponent.js
@@ -2,38 +2,47 @@ class LoadComponent {
 
     #loadComponentFunction(componentRequest, elementSeletor, typeInsertion) {
 
+        var identificator = "component_pitaya_" + Date.now();
+
         $.when($.get("components/" + componentRequest + ".html"))
                 .done(function (response) {
 
                     var parser = new DOMParser();
                     var document = parser.parseFromString(response, "text/html");
 
+                    /* separate the contents of component */
+                    var css = document.querySelector("style").innerHTML;
+                    var script = document.querySelector("script").innerHTML;
+                    var html = "<section id=\"" + identificator + "\">" + document.body.innerHTML + "</section>";
+
                     switch (typeInsertion) {
 
                         case 1:
-                            $("<style/>").text(document.querySelector("style").innerHTML).prependTo(elementSeletor);
-                            $("<script/>").text(document.querySelector("script").innerHTML).prependTo(elementSeletor);
-                            $(document.body.innerHTML).prependTo(elementSeletor);
+                            $(html).prependTo(elementSeletor);
+                            $("<style/>").text(css).prependTo($("#" + identificator));
+                            $("<script/>").text(script).prependTo($("#" + identificator));
                             break;
 
                         case 2:
-                            $("<style/>").text(document.querySelector("style").innerHTML).appendTo(elementSeletor);
-                            $("<script/>").text(document.querySelector("script").innerHTML).prependTo(elementSeletor);
-                            $(document.body.innerHTML).appendTo(elementSeletor);
+                            $(html).appendTo(elementSeletor);
+                            $("<style/>").text(css).appendTo($("#" + identificator));
+                            $("<script/>").text(script).appendTo($("#" + identificator));
                             break;
 
                     }
 
                 });
 
+        return identificator;
+
     }
 
     prependTo(componentRequest, elementSeletor) {
-        this.#loadComponentFunction(componentRequest, elementSeletor, 1);
+        return this.#loadComponentFunction(componentRequest, elementSeletor, 1);
     }
 
     appendTo(componentRequest, elementSeletor) {
-        this.#loadComponentFunction(componentRequest, elementSeletor, 2);
+        return this.#loadComponentFunction(componentRequest, elementSeletor, 2);
     }
 
 }
